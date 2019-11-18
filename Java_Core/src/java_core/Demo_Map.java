c48f2957e2942b480a2aaa787209c6e255fb5a86
dat <tatdat1997@gmail.com>
2019-11-18 16:18:36 +0700

    Create new branch: Branch_JavaCore

---
+++ Java_Core/src/java_core/Demo_Map.java
@@ -0,0 +1,34 @@
+package java_core;
+
+import java.util.*;
+
+public class Demo_Map {
+	public static void main(String[] args) {	
+		Map<Integer, String> hashMap = new HashMap<Integer, String>();
+        // add elements to hashMap
+        hashMap.put(1, "Java");
+        hashMap.put(5, "C++");
+        hashMap.put(6, "PHP");
+        hashMap.put(4, "Python");
+        // show hashMap
+        show(hashMap);
+        hashMap.forEach((key, value) -> {
+            System.out.println("Key : " + key + " Value : " + value);
+        });
+        // remove element
+        hashMap.remove(2);
+        // show hashMap after remove
+        System.out.println("After remove:");
+        // show hashMap
+        show(hashMap);
+ 
+    }
+ 
+    
+    public static void show(Map<Integer, String> hashMap) {
+        Set<Integer> keySet = hashMap.keySet();
+        for (Integer key : keySet) {
+            System.out.println(key + " " + hashMap.get(key));
+        }
+    }
+}
